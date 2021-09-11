0f689a3f87b3dd9a3a7eb21399112e92abb635c3
Ederson Domiciano <38039703+edersonrpd@users.noreply.github.com>
2021-09-11 16:09:54 -0300

    finalizando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,46 @@
 import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObjects';
 
 export const home = (req: Request, res: Response) => {
-    res.send('Home do controller');
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg',
+        },
+    });
     //res.render('pages/page)
 };
 
 export const dogs = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg',
+        },
+    });
     //res.render('pages/page)
 };
 
 export const cats = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg',
+        },
+    });
     //res.render('pages/page)
 };
 
 export const fishes = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg',
+        },
+    });
     //res.render('pages/page)
 };
